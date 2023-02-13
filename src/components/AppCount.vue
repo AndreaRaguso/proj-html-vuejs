557d07b4cd06244437331c2626f1421ca602d586
Andrea Raguso <info@andrearaguso.com>
2023-02-13 19:25:11 +0100

    section counter finita

---
+++ src/components/AppCount.vue
@@ -0,0 +1,54 @@
+<script>
+
+export default {
+    name: 'AppCount',
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
+
+<template>
+
+    <section id="counter">
+        <div class=" position-relative">
+            <div class=" position-absolute">
+                <img class="w-100" src="../assets/img/shape-top.png" alt="shapetop">
+            </div>
+            <div class="count">
+                <div class="content">
+                    <h1 class="text-center">Anidio Quick Facts</h1>
+                    <div class="d-flex my-5">
+                        <div class="card-count me-4">
+                            <h1><span id="n1"></span>+</h1>
+                            <h5>Projects done</h5>
+                        </div>
+                        <div class="card-count me-4">
+                            <h1><span id="n2"></span>+</h1>
+                            <h5>Happy Clients</h5>
+                        </div>
+                        <div class="card-count me-4">
+                            <h1><span id="n3"></span>+</h1>
+                            <h5>Team Members</h5>
+                        </div>
+                        <div class="card-count me-4">
+                            <h1><span id="n4"></span>+</h1>
+                            <h5>Years on the market</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="position-absolute shape-b">
+                <img class="w-100" src="../assets/img/shape-bottom.png" alt="shapebottom">
+            </div>
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppWorks.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+    name: 'AppWorks',
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
+
+<template>
+
+    <section id="works">
+        <div class="container-lg">
+            <div>Our Works</div>
+            <h2>Featured Productions</h2>
+            <p>Here's just a small of some of those projects that we'ew quite proud of. If you're looking for something specific feel free to get in contact with us.</p>
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
