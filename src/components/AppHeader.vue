8fc80bd440d5e21ed4f32e1b10eab31a80c3ad3e
Andrea Raguso <info@andrearaguso.com>
2023-02-13 13:47:46 +0100

    Header + Jumbotron Base

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,9 +13,72 @@ export default {
 
 <template>
 
+    <!-- Header  -->
+
+    <header>
+
+        <!-- Header Container -->
+
+        <div class="container-lg">
+
+            <!-- Header Nav -->
+
+            <nav class="d-flex justify-content-between align-items-center header-container">
+                <div>
+                    <img src="../assets/img/logo.png" alt="logo">
+                </div>
+                <div>
+                    <ul class="d-flex header-list align-items-center">
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">About Us</a>
+                        </li>
+                        <li>
+                            <a href="">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="">Our Process</a>
+                        </li>
+                        <li>
+                            <a href="">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="">Blog</a>
+                        </li>
+                        <li>
+                            <a href="">Contact</a>
+                        </li>
+                        <li>
+                            <a href="" class="button btn-back text-dark">Get a Quote</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+
+    <!-- Jumbotron -->
+
+    <section class="jumbo">
+        <div class="container-lg">
+            <div class="text-center jumbo-text">
+                <h5>Image, Create, Experience</h5>
+                <h1>We Animate</h1>
+                <h1>Whit Ninja-Like Precision</h1>
+                <p>We are a character-driven animation studio striving to make high-quality animations.</p>
+                <div>
+                    <a href="" class="button btn-back text-dark mx-4">Start a Project</a>
+                    <a href="" class="button text-white">Portfolio</a>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 </style>
