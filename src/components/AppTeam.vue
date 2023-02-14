f20cf731b201a84d4bc0eecb6a2120b7cec69437
Andrea Raguso <info@andrearaguso.com>
2023-02-14 09:38:19 +0100

    team section finita

--- src/components/AppTeam.vue
+++ src/components/AppTeam.vue
@@ -13,15 +13,50 @@ export default {
 
 <template>
 
-    <section id="team">
+    <section id="team" class=" position-relative">
+
+        <div class=" position-absolute">
+            <img src="../assets/img/shape.png" alt="">
+        </div>
+        <div class=" position-absolute shape2">
+            <img src="../assets/img/shape2.png" alt="">
+        </div>
 
         <div class=" container-lg">
-            <div class="text-team text-center mx-auto">
+            <div class="text-team text-center mx-auto mb-3">
                 <h5>Anido Magic Team Member</h5>
                 <h2>Let Us Animate Your Project</h2>
                 <p>We create new worlds! Let's collaborate and create engaging, effective, award-winning animations</p>
             </div>
-            <div class="cards-team d-flex">
+            <div class="cards-team d-flex justify-content-center">
+                <div class="card-team text-center me-3">
+                    <div class="team-img">
+                        <img src="../assets/img/team1.png" alt="team 1">
+                    </div>
+                    <div class="team-info">
+                        <h5 class=" fw-bold">Robert Coleman</h5>
+                        <p>Owner & Creative Director</p>
+                    </div>
+                    <div class="logos d-flex justify-content-center mb-3">
+                        <a href="#"><i class="fab fa-facebook-square fa-2x logo me-3"></i></a>
+                        <a href="#"><i class="fab fa-twitter fa-2x logo me-3"></i></a>
+                        <a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+                    </div>
+                </div>
+                <div class="card-team text-center me-3">
+                    <div class="team-img">
+                        <img src="../assets/img/team1.png" alt="team 1">
+                    </div>
+                    <div class="team-info">
+                        <h5 class=" fw-bold">Robert Coleman</h5>
+                        <p>Owner & Creative Director</p>
+                    </div>
+                    <div class="logos d-flex justify-content-center mb-3">
+                        <a href="#"><i class="fab fa-facebook-square fa-2x logo me-3"></i></a>
+                        <a href="#"><i class="fab fa-twitter fa-2x logo me-3"></i></a>
+                        <a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+                    </div>
+                </div>
                 <div class="card-team text-center">
                     <div class="team-img">
                         <img src="../assets/img/team1.png" alt="team 1">
@@ -30,11 +65,25 @@ export default {
                         <h5 class=" fw-bold">Robert Coleman</h5>
                         <p>Owner & Creative Director</p>
                     </div>
-                    <div class="d-flex justify-content-center">
-                        <i class="fa-brands fa-facebook"></i>
-                        <i class="fa-brands fa-twitter"></i>
-                        <i class="fa-brands fa-instagram"></i>
+                    <div class="logos d-flex justify-content-center mb-3">
+                        <a href="#"><i class="fab fa-facebook-square fa-2x logo me-3"></i></a>
+                        <a href="#"><i class="fab fa-twitter fa-2x logo me-3"></i></a>
+                        <a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
                     </div>
+                </div>
+            </div>
+            <div class="d-flex mt-4 align-items-center justify-content-center">
+                <div class="me-3 btn-team">
+                    <i class="fa-sharp fa-solid fa-arrow-left"></i>
+                </div>
+                <div class="dot-team">
+                    <i class="fa-solid fa-circle me-3"></i>
+                    <i class="fa-solid fa-circle me-3 active"></i>
+                    <i class="fa-solid fa-circle me-3"></i>
+                    <i class="fa-solid fa-circle"></i>
+                </div>
+                <div class="btn-team ms-3">
+                    <i class="fa-solid fa-arrow-right"></i>
                 </div>
             </div>
         </div>
